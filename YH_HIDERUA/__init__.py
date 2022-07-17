0b8afb1574a6e87e801b6d1f7603e2ec921a8954
Frynoodles <sanshuidesu@163.com>
2022-07-17 20:47:47 +0800

    迁移config.json至resource

--- YH_HIDERUA/__init__.py
+++ YH_HIDERUA/__init__.py
@@ -1,4 +1,12 @@
 import requests
+from lxml import etree
+
+YINGHUA = 'http://www.yinghuacd.com'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'Referer': 'http://www.yinghuacd.com/'
+}
 
 
 def __get_yhdm_url(showid):
@@ -11,11 +19,28 @@ def __get_yhdm_url(showid):
     """
     return f'http://www.yinghuacd.com/show/{showid}.html'
 
-def check_yhdm_list()->dict:
+
+def check_yhdm_list(showid_list: list[str]) -> dict:
     """
     检测数据表yhdm中的番剧是否更新，返回json数据\n
     {"result":"True","data":[...]}\n
     result对应该函数的执行结果，后面的data对应更新的数据
 
+    :param showid_list: showid列表
+    :type showid_list: list[str]
     :return: 函数执行结果和获得的数据
     """
+    for showid in showid_list:
+        pass
+
+
+def get_one_am_data(showid: str) -> dict:
+    resp = requests.get(YINGHUA + showid, headers=headers)
+    print(resp.text)
+    resp.close()
+    pass
+
+
+# 测试用
+if __name__ == '__main__':
+    pass
